f0270054628739cdce9f6f2ebeea0f69faa5a088
x0x11877 <wacit@gmail.com>
2021-04-27 15:25:51 +0800

    fix

---
+++ at/penshe_ad_info.js
@@ -0,0 +1,23 @@
+{
+    "status":1,
+    "T_APPID":"4798C2BED82F443CA629132C754FBC53",
+    "U_APPID":"6087889b5844f15425ecec30",
+    "TUTTi_APPID":"202012080DHUSFMY5JN13BLI2AOQC8GZ",
+    "TUTTi_ScrKey":"9f3fca743f7ba2999d6882898dbb3343",
+    "CSJ_APPID":"5165468",
+    "APPTUTTi_CSJ_APPID":"5165468",
+    "Switch_APPTUTTi_CSJ_APPID":"5165468",
+    "Switch_CSJ_APPID":"5165468",
+    
+    "Switch_CSJ_Video":"946056741",
+    "Switch_APPTUTTi_CSJ_Video":"946056741",
+    "CSJ_Video":"946056741",
+    "CSJ_Splash":"887467249",
+    "CSJ_Interstitial":"946056738",
+    
+    "APPTUTTi_CSJ_Video":"946056741",
+    "APPTUTTi_CSJ_Splash":"887467249",
+    "APPTUTTi_CSJ_Video_Express":"946056741",
+    "APPTUTTi_CSJ_Interstitial":"946056738",
+    "APPTUTTi_CSJ_Banner":"887467249"
+    }
