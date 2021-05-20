4926c62313e6976523a9cb2136ee272ccbf001dd
x0x11877 <wacit@gmail.com>
2021-05-20 16:07:51 +0800

    add tt site

---
+++ at/tt_outsite_ad_info.js
@@ -0,0 +1,22 @@
+{
+    "status":1,
+    "T_APPID":"4798C2BED82F443CA629132C754FBC53",
+    "U_APPID":"607fef8d5844f15425e20872",
+    "TUTTi_APPID":"202012080DHUSFMY5JN13BLI2AOQC8GZ",
+    "TUTTi_ScrKey":"9f3fca743f7ba2999d6882898dbb3343",
+    "CSJ_APPID":"5161818",
+    "APPTUTTi_CSJ_APPID":"5161818",
+    "Switch_APPTUTTi_CSJ_APPID":"5161818",
+    "Switch_CSJ_APPID":"5161818",
+    
+    "Switch_CSJ_Video":"946129391",
+    "Switch_APPTUTTi_CSJ_Video":"946129391",
+    "CSJ_Video":"946129391",
+    "CSJ_Splash":"887476976",
+    "CSJ_Interstitial":"946129393",
+    
+    "APPTUTTi_CSJ_Video":"946129391",
+    "APPTUTTi_CSJ_Splash":"887476976",
+    "APPTUTTi_CSJ_Video_Express":"946129391",
+    "APPTUTTi_CSJ_Interstitial":"946129393"
+    }
